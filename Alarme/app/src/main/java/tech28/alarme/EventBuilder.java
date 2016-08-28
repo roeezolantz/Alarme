f041c30c28fc0dfa4c4754ad1b4927ab23ccbd85
Roee Zolantz <xdlunatic@gmail.com>
2016-08-28 04:16:16 -0700

    Added navigation to event builder activity

---
+++ Alarme/app/src/main/java/tech28/alarme/EventBuilder.java
@@ -0,0 +1,13 @@
+package tech28.alarme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventBuilder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_builder);
+    }
+}
--- Alarme/app/src/main/java/tech28/alarme/Main2Activity.java
+++ Alarme/app/src/main/java/tech28/alarme/Main2Activity.java
@@ -1,6 +1,7 @@
 package tech28.alarme;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,8 @@ public class Main2Activity extends AppCompatActivity {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(cnt, "LOL", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(cnt, EventBuilder.class);
+                startActivity(intent);
             }
         });
     }
