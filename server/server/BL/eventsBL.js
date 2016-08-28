a21f0d91b88459b718a945f90402bf666e62e398
liorhirsch <liorhirsch@gmail.com>
2016-08-29 01:10:26 +0300

    Events : 1. get all events of user. 2. find all events near the scepcified user.

---
+++ server/server/BL/eventsBL.js
@@ -0,0 +1,47 @@
+var MongoAccess = require("../Dal/MongoQuery");
+var DBName = 'Events';
+
+var eventsBL = {};
+
+eventsBL.eventsOfUser = function(req, res)
+{
+    MongoAccess.getDB(function(db)
+    {
+        db.collection(DBName).find({from : req.params.fromTelNumber})
+        .toArray(function(err, docs) 
+        {
+            res.send(docs);
+            db.close();
+        })
+    });    
+};
+
+eventsBL.findEventsNearMe = function(req, res) 
+{
+    req.body.location = JSON.parse(req.body.location);
+
+    MongoAccess.getDB(function(db)
+    {
+        var collection = db.collection(DBName);
+        collection.find( 
+        { 
+            "from" : req.body.userTelNum,
+            "location":{                
+                    "$near":
+                    {
+                        "$geometry":
+                        {
+                            "type" : "Point" ,
+                            "coordinates": [ req.body.location.x , req.body.location.y ]
+                        },
+                        "$maxDistance" : 100,
+                    }
+            }
+        }).toArray(function(err, results)
+        {
+            res.send(results);    
+        });
+  })
+};
+
+module.exports = eventsBL;
--- server/server/routes/events.js
+++ server/server/routes/events.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var eventsBL = require("../BL/eventsBL");
 
 /* GET users listing. */
 router.get('/:fromTelNumber', function(req, res, next) {
-  res.send(req.params.fromTelNumber);
+    eventsBL.eventsOfUser(req, res);
 });
+
+router.post('/findEventsNearMe', function(req, res, next) {
+    eventsBL.findEventsNearMe(req, res);
+});
+
 
 module.exports = router;
