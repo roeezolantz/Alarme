6efafca7de5ff6b420d5ef2c0b7b7d6472ad49e4
Gilwe <giladwss@gmail.com>
2016-08-29 03:28:10 +0300

    balls

--- server/server/BL/eventsBL.js
+++ server/server/BL/eventsBL.js
@@ -44,4 +44,47 @@ eventsBL.findEventsNearMe = function(req, res)
   })
 };
 
+// Required info: commanderNumber, message
+eventsBL.createEvent = function(req,res)
+{
+    MongoAccess.getDB(function(db)
+    {
+        // Get commander's soldiers
+        db.collection(DBName).find({ commanderId: req.body.commanderNumber}, { soldiers : 1 , _id : 0})
+            .toArray(function(err, soldiers)
+            {
+                if (soldiers == null)
+                {
+                    res.send("This commander has no soldiers");
+                }
+                // In case he has more than 1
+                else
+                    {
+                        soldiers.forEach(function(soldier)
+                        {
+                        var homeAddress = db.collection(DBName)
+                            .find({from: soldier}, {homeAddress : 1, _id : 0})
+                            .toArray(function(err,homeAddress)
+                            {
+                                var event = {
+                                    from: req.body.commanderNumber,
+                                      to: soldier,
+                                   location: homeAddress,
+                                    message: req.body.message
+                                }
+
+                                db.collection(DBName).insertOne(event, function(err,result)
+                                {
+                                    db.close();
+                                })
+                            });
+
+                        });
+                    }
+                
+            });
+        });
+    };   
+
+
 module.exports = eventsBL;
